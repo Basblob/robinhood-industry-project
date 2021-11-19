2a4a662d5666e6eb0b16cc76122663e9295fc180
Sasha <sashajsirota@gmail.com>
2021-11-18 22:31:22 -0500

    file structure and cryptoPage boiler

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,17 @@
 import "./App.scss";
+import { React, Route, Routes, BrowserRouter as Router } from "react";
+import Home from "./pages/homePage";
+import Crypto from "./pages/cryptoPage";
 
 function App() {
-  return <div></div>;
+  return (
+    <Router>
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route path="/crypto" element={<Crypto />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
